4e416388938c2d77374fa1e47f0de1e81790493f
kristofferdamborg@gmail.com <kristofferdamborg@gmail.com>
2017-04-10 22:39:20 +0200

    Initial commit

---
+++ Assignment 1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Assignment_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            var context = new IdentityDbContext();
+            var users = context.Users.ToList();
+
+            ViewBag.Users = users;
+            return View(users);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Assignment 1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment_1.Startup))]
+namespace Assignment_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
